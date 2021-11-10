5c39ad9352dc2efe6fa4cde5aed9a40577d2b205
Dagim Shimelis <dagimshimelis1992@gmail.com>
2021-11-10 13:59:02 +0300

    edited expense list
    -changed the view of the expense list section
    -added informative comments
    -divided code into small section with comments
    -

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+//-----------------------------expence list holder------------------------------
   //TODO: let the user add expenses dynamicaly through the app
   final List<Expenses> expenses = [
     Expenses(
@@ -31,6 +32,7 @@ class MyHomePage extends StatelessWidget {
       date: DateTime.now(),
     )
   ];
+//--------------------------##-expence list holder-##---------------------------
 
   @override
   Widget build(BuildContext context) {
@@ -38,14 +40,12 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Xpenses"),
       ),
-      body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-
+      body: Column(mainAxisAlignment: MainAxisAlignment.start,
           // ignore: prefer_const_literals_to_create_immutables
           children: <Widget>[
+//-----------------------------expence chart------------------------------------
             Container(
-              width: double.infinity,
+              // width: double.infinity,
               child: Card(
                 child: Text(
                     "CHART"), //TODO: add chart content instead of Text widget
@@ -53,22 +53,65 @@ class MyHomePage extends StatelessWidget {
                 color: Colors.blue,
               ),
             ),
+//--------------------------##-expence chart-##---------------------------------
+
+//-----------------------------expence list-------------------------------------
             Column(
               children: expenses.map((ex) {
                 //TODO: decorate the card to look better
                 //change the date format to a more readable form
 
+                //this card will hold the cahrts that will show the weeky expenses
                 return (Card(
                   child: Row(children: <Widget>[
-                    Text(ex.amount.toString()),
-                    Column(children: <Widget>[
-                      Text(ex.title),
-                      Text(ex.date.toString()),
-                    ])
+                    Container(
+                      margin: EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 15,
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.purple,
+                          width: 2,
+                        ),
+                      ),
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        "\$${ex.amount}",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple,
+                        ),
+                      ),
+                    ),
+
+                    //this column will hold the lost of all expenses
+                    Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text(
+                            ex.title,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18,
+                                color: Colors.black),
+                          ),
+                          Text(
+                            ex.date.toString(),
+                            style: TextStyle(
+                              fontWeight: FontWeight.w100,
+                              fontSize: 10,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ])
                   ]),
                 ));
               }).toList(),
             ),
+//--------------------------##-expence list-##----------------------------------
           ]),
     );
   }
